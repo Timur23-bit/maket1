ff6ca44695b66006b9269e21b3a06c1a3cdc5b90
Timur23-bit <62034798+Timur23-bit@users.noreply.github.com>
2020-04-11 22:46:31 +0300

    Добавил макет блока 1.5

--- showall.js
+++ showall.js
@@ -1,22 +1,22 @@
-var showAll = document.querySelector('.show-all');
-var closeAll =  document.querySelector('.close-show-all');
-var cardList = document.querySelector('.brands__list');
-var card = cardList.children;
+let showAll = document.querySelector('.show-all');
+let closeAll =  document.querySelector('.close-show-all');
+let cardList = document.querySelector('.swiper-wrapper');
+let card = cardList.children;
 showAll.addEventListener('click', function(evt){
     evt.preventDefault();
 
     showAll.classList.add('--hidden');
     closeAll.classList.remove('--hidden');
-    for (var i = 1; i <= card.length; i++){
+    for (let i = 1; i <= card.length; i++){
         card[card.length-i].classList.add('--show')
     }
-})
+});
 
 closeAll.addEventListener('click', function(evt){
     evt.preventDefault();
     closeAll.classList.add('--hidden');
     showAll.classList.remove('--hidden');
-    for (var i = 1; i <= 5; i++){
+    for (let i = 1; i <= 5; i++){
         card[card.length-i].classList.remove('--show')
     }
-})
+});
