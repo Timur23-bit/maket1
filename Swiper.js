9ac08a0b3e1d62a77ba95db0669bd464511a0817
Timur23-bit <62034798+Timur23-bit@users.noreply.github.com>
2020-04-11 23:10:39 +0300

    Добавил макет блока 1.5

---
+++ Swiper.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper('.swiper-container', {
+            slidesPerView: 'auto',
+            spaceBetween:0,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
