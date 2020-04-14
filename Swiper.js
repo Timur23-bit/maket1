e4953efe142e25458e78f7394a8ede006d230200
Timur23-bit <62034798+Timur23-bit@users.noreply.github.com>
2020-04-14 21:10:59 +0300

    Добавил макет блока 1.5

--- Swiper.js
+++ Swiper.js
@@ -1,8 +1,29 @@
-let swiper = new Swiper('.swiper-container', {
+if (window.innerWidth < 768) {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+}
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 768){
+        swiper = new Swiper('.swiper-container', {
+            updateOnWindowResize: true,
             slidesPerView: 'auto',
             spaceBetween:0,
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true,
             },
+
         });
+        swiper.pagination.update();
+    } else if (window.innerWidth >= 768){
+        swiper.pagination.update();
+        swiper.destroy(true, false);
+        swiper.update();
+    }
+});
